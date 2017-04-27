82c7d8d05d550c882e0986cb2050e93e10859672
Aluno <Aluno@10.151.31.116>
2017-04-27 17:09:30 -0300

    Terminei os vendedores, coloquei fotos novas, fiz paginas de gatos e cachorros.

--- FeedControl/js/controller.js
+++ FeedControl/js/controller.js
@@ -1,14 +1,41 @@
-// Ionic Starter App
+var app = angular.module('contactApp', []);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('myApp', []);
 
-app.controller('MainCtrl', function ($scope, $http) {
-
- 
-
+app.controller('ContactController', function ($scope, $http) {
+    $scope.result = 'hidden'
+    $scope.resultMessage;
+    $scope.formData; //formData is an object holding the name, email, subject, and message
+    $scope.submitButtonDisabled = false;
+    $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+    $scope.submit = function(contactform) {
+        
+        $scope.submitted = true;
+        $scope.submitButtonDisabled = true;
+        if (contactform.$valid) {
+            
+            $http({
+                method  : 'POST',
+                url     : 'php/contact-form.php',
+                data    : $.param($scope.formData),  //param method from jQuery
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+            }).then(function(data){
+                console.log(data);
+                if (data.success) { //success comes from the return json object
+                    $scope.submitButtonDisabled = true;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-success';
+                } else {
+                    $scope.submitButtonDisabled = false;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-danger';
+                }
+            });
+        } else {
+            $scope.submitButtonDisabled = false;
+            $scope.resultMessage = 'Failed :( Please fill out all the fields.';
+            $scope.result='bg-danger';
+        }
+    }
 });
 
 
